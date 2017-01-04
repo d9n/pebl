5890a5d3a245346c33b4fcf4cadc0b591b02237d
David Herman <d9n.coding@gmail.com>
2017-01-03 22:19:16 -0800

    Simplify property code using unsafe blocks

--- src/property.rs
+++ src/property.rs
@@ -1,53 +1,96 @@
-use rcref::RcRef;
-
 use std::fmt;
+use std::marker::PhantomData;
 
-pub trait Getter<T> {
-    fn get<F: Fn(&T)>(&self, f: F);
+struct Value<T: PartialEq> {
+    value: T,
 }
 
-pub trait Setter<T> {
-    fn set(&mut self, value: T);
+impl<T: PartialEq> Value<T> {
+    fn new(value: T) -> Value<T> {
+        Value { value: value }
+    }
 }
 
 pub struct Property<T: PartialEq> {
-    value: RcRef<T>,
+    inner_value: Value<T>,
 }
 
 impl<T: PartialEq> Property<T> {
-    pub fn new(value: T) -> Self {
-        return Property { value: RcRef::new(value) };
+    pub fn new(value: T) -> Property<T> {
+        Property { inner_value: Value::new(value) }
+    }
+
+    pub fn get(&self) -> &T {
+        &self.inner_value.value
+    }
+
+    pub fn set(&mut self, value: T) {
+        self.inner_value.value = value;
     }
 }
 
 impl<T: PartialEq + Default> Property<T> {
-    pub fn reset(&mut self) {
+    pub fn clear(&mut self) {
         self.set(Default::default());
+    }
+}
+
+pub struct PropertyRef<'a, T: 'a + PartialEq> {
+    value_ptr: *const Value<T>,
+    phantom: PhantomData<&'a T>,
+
+}
+
+impl<'a, T: 'a + PartialEq> PropertyRef<'a, T> {
+    pub fn new(p: &'a Property<T>) -> PropertyRef<'a, T> {
+        PropertyRef { value_ptr: &p.inner_value, phantom: PhantomData }
+    }
+
+    pub fn get(&self) -> &T {
+        unsafe { & (*self.value_ptr).value }
+    }
+}
+
+pub struct PropertyMutRef<'a, T: 'a + PartialEq> {
+    value_ptr: *mut Value<T>,
+    phantom: PhantomData<&'a T>,
+}
+
+impl<'a, T: 'a + PartialEq> PropertyMutRef<'a, T> {
+    pub fn new(p: &'a mut Property<T>) -> PropertyMutRef<'a, T> {
+        PropertyMutRef { value_ptr: &mut p.inner_value, phantom: PhantomData }
+    }
+
+    pub fn get(&self) -> &T {
+        unsafe { & (*self.value_ptr).value }
+    }
+
+    pub fn set(&mut self, value: T) {
+        let value_ref = unsafe { &mut *self.value_ptr };
+        value_ref.value = value;
     }
 }
 
 impl<T: PartialEq + Default> Default for Property<T> {
     fn default() -> Self {
-        return Property::new(Default::default());
-    }
-}
-
-impl<T: PartialEq> Getter<T> for Property<T> {
-    fn get<F: Fn(&T)>(&self, f: F) {
-        self.value.peek(f);
-    }
-}
-
-impl<T: PartialEq> Setter<T> for Property<T> {
-    fn set(&mut self, value: T) {
-        if *self.value.borrow() != value {
-            *self.value.borrow_mut() = value;
-        }
+        Property::new(Default::default())
     }
 }
 
 impl<T: PartialEq + fmt::Debug> fmt::Debug for Property<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Property {{ {:?} }}", self.value)
+        write!(f, "Property {{ {:?} }}", self.get())
+    }
+}
+
+impl<'a, T: PartialEq + fmt::Debug> fmt::Debug for PropertyRef<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "PropertyRef {{ {:?} }}", self.get())
+    }
+}
+
+impl<'a, T: PartialEq + fmt::Debug> fmt::Debug for PropertyMutRef<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "PropertyMutRef {{ {:?} }}", self.get())
     }
 }
--- tests/property.rs
+++ tests/property.rs
@@ -9,61 +9,79 @@ use pebl::prelude::*;
 #[test]
 fn default_properties() {
     let property = Property::<i32>::default();
-    property.get(|val| assert_that(val).is_equal_to(&0));
+    assert_that(property.get()).is_equal_to(&0);
 
     let property = Property::<bool>::default();
-    property.get(|val| assert_that(val).is_equal_to(false));
+    assert_that(property.get()).is_false();
 
     let property = Property::<f64>::default();
-    property.get(|val| assert_that(val).is_equal_to(0.0));
+    assert_that(property.get()).is_equal_to(&0.0);
 
     let property = Property::<Option<i32>>::default();
-    property.get(|val| assert_that(val).is_none());
+    assert_that(property.get()).is_none();
 
     let property = Property::<Rc<i32>>::default();
-    property.get(|val| assert_that(&**val).is_equal_to(&0));
+    assert_that(& **property.get()).is_equal_to(&0);
 }
 
 #[test]
 fn property_simple_get() {
     let p = Property::new(10);
-    p.get(|val| assert_that(&val).is_equal_to(&10));
-    p.get(|val| assert_that(&val).is_equal_to(&10));
+    assert_that(p.get()).is_equal_to(&10);
+    assert_that(p.get()).is_equal_to(&10);
 }
 
 #[test]
 fn property_simple_set() {
     let mut p = Property::new(10);
     p.set(20);
-    p.get(|val| assert_that(val).is_equal_to(&20));
+    assert_that(p.get()).is_equal_to(&20);
 }
 
 #[test]
 fn property_clear() {
-    let mut property = Property::new(10);
-    property.reset();
-    property.get(|val| assert_that(val).is_equal_to(&0));
+    let mut p = Property::new(10);
+    p.clear();
+    assert_that(p.get()).is_equal_to(&0);
 
-    let mut property = Property::new(Option::from("Hello"));
-    property.reset();
-    property.get(|val| assert_that(val).is_none());
+    let mut p = Property::new(Option::from("Hello"));
+    p.clear();
+    assert_that(p.get()).is_none();
+}
+
+#[test]
+fn property_ref() {
+    let p = Property::new(123);
+    let p_ref = PropertyRef::new(&p);
+
+    assert_that(p_ref.get()).is_equal_to(&123);
+}
+
+#[test]
+fn property_mut_ref() {
+    let mut p = Property::new(123);
+    let mut p_mut_ref = PropertyMutRef::new(&mut p);
+
+    assert_that(p_mut_ref.get()).is_equal_to(&123);
+    p_mut_ref.set(321);
+    assert_that(p_mut_ref.get()).is_equal_to(&321);
 }
 
 #[test]
 fn property_takes_ownership() {
     let range: Vec<_> = (0..100).collect();
-    let p = Property::new(range);
+    let _ = Property::new(range);
 //    let range_already_taken = range; // As expected, uncommenting causes compile error
-
-    p.get(|val| assert_that(& *val).has_length(100));
-    // Assert again, verifying vector still available for reading
-    p.get(|val| assert_that(& *val).has_length(100));
 }
 
 #[test]
-fn property_implements_debug() {
-    let p = Property::new(42);
-    let p_string = format!("{:?}", p);
+fn property_classes_implement_debug() {
+    let mut p = Property::new(42);
 
+    let p_string = format!("{:?}", p);
+    assert_that(&p_string.len()).is_greater_than(0);
+    let p_string = format!("{:?}", PropertyRef::new(&p));
+    assert_that(&p_string.len()).is_greater_than(0);
+    let p_string = format!("{:?}", PropertyMutRef::new(&mut p));
     assert_that(&p_string.len()).is_greater_than(0);
 }
