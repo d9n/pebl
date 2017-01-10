1b30fc1781afca185da20ff17959978bfdf7fe1e
David Herman <d9n.coding@gmail.com>
2017-01-09 22:05:36 -0800

    New PropertyList for storing and iterating property pointers

--- src/property.rs
+++ src/property.rs
@@ -181,6 +181,15 @@ impl<T: PartialEq> PropertyPtr<T> {
     }
 }
 
+impl<T: PartialEq> Clone for PropertyPtr<T> {
+    fn clone(&self) -> Self {
+        PropertyPtr {
+            value_cell_ptr: self.value_cell_ptr,
+            weak_borrow_counts: self.weak_borrow_counts.clone(),
+        }
+    }
+}
+
 impl<T: PartialEq + fmt::Debug> fmt::Debug for Property<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Property {{ {:?} }}", self.get())
@@ -205,5 +214,116 @@ impl<T: PartialEq + fmt::Debug> fmt::Debug for PropertyPtr<T> {
             None => write!(f, "*Property {{ null }}"),
             Some(ref p) => write!(f, "*Property {{ {:?} }}", p.get())
         };
+    }
+}
+
+pub struct PropertyList<T : PartialEq> {
+    // Outer RefCell so that we can clean the vec in notify_invalidated
+    items: RefCell<Vec<PropertyPtr<T>>>,
+}
+
+pub struct PropertyListIterator<'a, T: 'a + PartialEq> {
+    property_list: &'a PropertyList<T>,
+    index: usize,
+}
+
+impl<T: PartialEq> PropertyList<T> {
+    pub fn new() -> Self {
+        PropertyList::<T> { items: RefCell::new(Vec::new()) }
+    }
+
+    pub fn push(&mut self, property: &Property<T>) {
+        self.items.borrow_mut().push(PropertyPtr::new(property));
+    }
+
+    pub fn iter(&self) -> PropertyListIterator<T> {
+        return PropertyListIterator { property_list: &self, index: 0 }
+    }
+
+    #[cfg(test)]
+    fn len(&self) -> usize {
+        self.items.borrow().len()
+    }
+
+    fn clean(&self) {
+        self.items.borrow_mut().retain(|p_ptr| { return p_ptr.get().is_some() });
+    }
+}
+
+impl<'a, T: 'a + PartialEq> Iterator for PropertyListIterator<'a, T> {
+    type Item = PropertyPtr<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        {
+            let items = self.property_list.items.borrow();
+
+            loop {
+                if self.index >= items.len() {
+                    break;
+                }
+
+                let p_ptr = &items[self.index];
+                self.index += 1;
+
+                if p_ptr.get().is_some() {
+                    return Some(p_ptr.clone());
+                }
+            }
+        }
+
+        self.property_list.clean(); // Only do after items is out of scope
+        None
+    }
+}
+
+impl<'a, T: 'a + PartialEq> IntoIterator for &'a PropertyList<T> {
+    type Item = PropertyPtr<T>;
+    type IntoIter = PropertyListIterator<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+#[cfg(test)]
+mod private_api_tests {
+    extern crate spectral;
+
+    use self::spectral::prelude::*;
+    use super::*;
+
+    #[test]
+    fn property_list_is_cleaned_after_iterated() {
+        let mut list = PropertyList::<i32>::new();
+        let p1 = Property::<i32>::new(10);
+        list.push(&p1);
+        {
+            let p2 = Property::<i32>::new(20);
+            list.push(&p2);
+
+            assert_that(&list.len()).is_equal_to(&2);
+        }
+        assert_that(&list.len()).is_equal_to(&2);
+
+        for _ in list.iter() {} // Once iteration is done, dead weak refs are removed
+        assert_that(&list.len()).is_equal_to(&1);
+    }
+
+    #[test]
+    fn weak_list_ref_can_be_used_in_for_loop() {
+        let mut list = PropertyList::<i32>::new();
+        let p1 = Property::<i32>::new(0);
+        let p2 = Property::<i32>::new(1);
+        let p3 = Property::<i32>::new(2);
+        list.push(&p1);
+        list.push(&p2);
+        list.push(&p3);
+
+        let mut i = 0;
+        for p in &list {
+            assert_that(&i).is_equal_to(p.get().unwrap().get());
+            i += 1;
+        }
+        assert_that(&i).is_equal_to(3);
     }
 }
